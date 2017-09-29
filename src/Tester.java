daad04fa28c1f96fce0a90f05934b6ccb4742ad9
David Dobervich <david.dobervich@gmail.com>
2017-09-29 16:36:21 -0700

    resolve merge conflicts

--- src/Tester.java
+++ src/Tester.java
@@ -1,12 +1,43 @@
 
 public class Tester {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		String cipherText = Cipher.loadFileAsString("cipherText1.txt");
-		System.out.println("CIPHER TEXT IS:\n" + cipherText);
-		
-		String plainText = Cipher.rotationCipherDecrypt(cipherText, -20);
-		System.out.println("PLAINTEXT IS: \n" + plainText);
-		
+		System.out.println("Cipher text 1:\n" + cipherText);
+
+		int rotAmount = Cipher.bruteForceCrackRotation(cipherText);
+		System.out.println("CRACKED! Rotation amount is " + rotAmount);
+
+		String plainText = Cipher.rotationCipherDecrypt(cipherText, rotAmount);
+
+		System.out.println("PLAINTEXT IS: " + plainText);
+
+		// -========== CRACK CIPHER 2 ===========-
+		// cipherText2.txt was encoded with a 2-letter password
+		// ---------------------------------------
+
+		// cipherText = Cipher.loadFileAsString("cipherText2.txt");
+		// System.out.println("Cipher text 2:\n" + cipherText);
+		//
+		// password = Cipher.bruteForceCrackLength2Password(cipherText);
+		// System.out.println("CRACKED! Password is " + password);
+		//
+		// plainText = Cipher.vigenereCipherDecrypt(cipherText, password);
+		//
+		// System.out.println("PLAINTEXT IS: " + plainText);
+
+		// -========== CRACK CIPHER 3 ===========-
+		// cipherText2.txt was encoded with a 3-letter password
+		// ---------------------------------------
+
+		// cipherText = Cipher.loadFileAsString("cipherText2.txt");
+		// System.out.println("Cipher text 3:\n" + cipherText);
+		//
+		// password = Cipher.bruteForceCrackLength2Password(cipherText);
+		// System.out.println("CRACKED! Password is " + password);
+		//
+		// plainText = Cipher.vigenereCipherDecrypt(cipherText, password);
+		//
+		// System.out.println("PLAINTEXT IS: " + plainText);
 	}
 }
